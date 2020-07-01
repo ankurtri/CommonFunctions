510bc1cab7c6a850ed7e55a69b5809aee12c38b2
Ankur Trivedi <ankurt.kanpur@gmail.com>
2020-07-01 19:25:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,6 +9,22 @@ from email.mime.multipart import MIMEMultipart
 import os
 import glob
 
+from dateutil.parser import parse
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try: 
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
+
 def sas_to_csv():
     fileLists = glob.glob("*.sas7bdat")
 
